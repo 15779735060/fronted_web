7b8ad9de2783cf283a3b52625609c52885ade1f5
YanXiuFei <495654820@qq.com>
2023-06-18 22:05:05 +0800

    原型和函数、函数作用域链详解

--- docs/JavaScript/sidebar.js
+++ docs/JavaScript/sidebar.js
@@ -16,10 +16,36 @@ module.exports = {
       children: [{
         title: '变量声明方式',
         path: '/JavaScript/变量声明方式.md'
-      }, {
+      },
+      {
         title: '数据类型说明',
         path: '/JavaScript/数据类型说明.md'
-      }] //2级页面，第一个设置为空字符串时，显示的是README.md的内容
+      },
+      {
+        title: '操作符',
+        path: '/JavaScript/操作符.md'
+      },
+      {
+        title: '控制流语句',
+        path: '/JavaScript/控制流语句.md'
+      },
+      {
+        title: '函数与函数作用域链',
+        path: '/JavaScript/函数与函数作用域链.md'
+      },
+      {
+        title: '闭包与递归',
+        path: '/JavaScript/闭包与递归.md'
+      },
+      {
+        title: '原型和原型链',
+        path: '/JavaScript/原型和原型链.md'
+      },
+      {
+        title: '各数据类型的原型方法',
+        path: '/JavaScript/各数据类型的原型方法.md'
+      }
+      ] //2级页面，第一个设置为空字符串时，显示的是README.md的内容
     },
   ]
 }
